111cf7a69f20fd1ac27bd8a76576e4bda1f82fed
alloy <eloy.de.enige@gmail.com>
2009-08-25 03:32:29 -0700

    Options, options, options…

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -20,8 +20,6 @@ module ProjectsHelper
   #       <% end %>
   #     </div>
   #   <% end -%>
-
-
   def generate_html(form_builder, method, options = {})
     options[:object] ||= form_builder.object.class.reflect_on_association(method).klass.new
     options[:partial] ||= method.to_s.singularize
@@ -30,15 +28,9 @@ module ProjectsHelper
     form_builder.fields_for(method, options[:object], :child_index => 'NEW_RECORD') do |f|
       render(:partial => options[:partial], :locals => { options[:form_builder_local] => f })
     end
-
   end
-
 
   def generate_template(form_builder, method, options = {})
-    escape_javascript generate_html(form_builder, method, options = {})
+    escape_javascript generate_html(form_builder, method, options)
   end
-
-
-
 end
-
